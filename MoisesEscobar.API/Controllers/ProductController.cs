f6fc1adcd592d8eb9de7955f969ffdea6ec5715d
Moises Escobar <moisesescobar50@outlook.com>
2024-12-10 11:18:33 -0600

    Nuevos proyectos

---
+++ MoisesEscobar.API/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoisesEscobar.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        [HttpGet(Name = "GetProducts")]
+        public IEnumerable<Product> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Product
+            {
+                Name = "Coca-Cola Lata " + index,
+                Description = "Bebida - La constancia",
+                Price = 0.5m,
+                UrlImage = "https://m.media-amazon.com/images/I/51v8nyxSOYL._SL1500_.jpg"
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ MoisesEscobar.API/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace MoisesEscobar.API
+{
+    public class Product
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string UrlImage { get; set; } = string.Empty;
+    }
+}
